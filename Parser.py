a5f705f9039578b595ac866cf81f3975196309f7
Danil <165506422+nikewalce@users.noreply.github.com>
2024-11-18 13:07:33 +0300

    Add files via upload

---
+++ Parser.py
@@ -0,0 +1,138 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
+import json
+import Catalog
+import telebot
+
+class Parser():
+
+    def __init__(self, userId):
+        self.bot = telebot.TeleBot(')
+        self.HEADERS = {'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.190 Safari/537.36'}
+        self.HOST = 'https://366.ru'
+        self.userId = userId
+
+    def get_URL(self,name_subcat):
+        cat = Catalog.parse()
+        for href in cat:
+            if href['text'] == name_subcat:
+                return href['href']
+
+    def get_html(self, url, params=None):
+        r = requests.get(url, headers=self.HEADERS,params=params)
+        return  r
+
+    def get_pages_count(self,html):
+        soup = BeautifulSoup(html, 'html.parser')  # формируются объекты
+        pagination = soup.find_all('a',class_='b-pagination__item')
+        if pagination:
+            return int(pagination[-2].get_text())#срез(предпоследний элемент)
+        else:
+            return 1
+
+    def get_content(self,html):
+        soup = BeautifulSoup(html, 'html.parser')  # формируются объекты
+        card = soup.find_all('div', class_='listing_product')
+
+        listOfCard = []
+
+        for item in card:
+            #a = ' '.join(div[i].get_text().split())
+            listOfCard.append({
+                'title': self.__Title(item.find('a', class_='listing_product__title')),
+                'manufacturer': self.__Manufacturer(item.find('div', class_='listing_product__manufacturer')),
+                'elipsis': self.__Elipsis(item.find('div', class_='i-text-ellipsis')),
+                'NumberOfPharmacies': self.__NumberOfPharmacies(item.find('div', class_='c-prod-item--product-text')),
+                'price': self.__Price(item.find('span', class_='price')),
+                'link': self.__Link(item.find('a', class_='listing_product__title')),
+                'image': self.__Image(item.find('img', class_='lazyload'))
+            })
+        return listOfCard
+
+    def __Image(self,item):
+        if item:
+            item = item.get('data-src')
+        else:
+            item = None
+        return item
+
+    def __Link(self,item):
+        if item:
+            item = self.HOST + item.get('href')
+        else:
+            item = None
+        return item
+
+
+    def __Price(self,item):
+        if item:
+            item = ' '.join(item.get_text().split())
+        else:
+            item = 'None'
+        return item
+
+    def __Title(self,item):
+        if item:
+            item = ' '.join(item.get_text().split())
+        else:
+            item = 'None'
+        return item
+
+    def __Manufacturer(self,item):
+        if item:
+            item = ' '.join(item.get_text().split())
+        else:
+            item = 'None'
+        return item
+
+    def __Elipsis(self,item):
+        if item:
+            item = ' '.join(item.get_text().split())
+        else:
+            item = 'None'
+        return item
+
+    def __NumberOfPharmacies(self,item):
+        if item:
+            item = ' '.join(item.get_text().split())
+        else:
+            item = 'None'
+        return item
+
+    def save_file(self, items, path):
+        with open('Catalog\\'+path, 'w', encoding='utf-8') as file:
+            writer =csv.writer(file, delimiter=',')#delimetr=';' - разделитель, для excel открытия
+            writer.writerow(['Название', 'Производитель', 'Содержание', 'Количество на складе', 'Цена', 'Ссылка', 'Ссылка на изображение'])
+            for item in items:
+                writer.writerow([item['title'],item['manufacturer'],item['elipsis'], item['NumberOfPharmacies'],item['price'],item['link'],item['image']])
+
+    def read_json_subcat(self):
+        with open("subcat.json", "r", encoding='utf-8') as file:
+            data = json.loads(file.read())
+            return data
+
+
+    def parse(self, category, name_file):
+        #print(read_json_subcat())
+        idMessage = ''
+        try:
+            URL = self.get_URL(category).strip()
+            FILE = name_file
+            html = self.get_html(URL)
+            if html.status_code == 200:
+                medications = []
+                pages_count = self.get_pages_count(html.text)
+                for page in range(1,pages_count + 1):
+                    if idMessage != '':
+                        self.bot.delete_message(self.userId, idMessage.id)
+                    print(f'Парсинг страницы {page} из {pages_count}')
+                    idMessage = self.bot.send_message(self.userId, text=f'Парсинг страницы {page} из {pages_count}')
+                    html = self.get_html(URL,params={'page':page})
+                    medications.extend(self.get_content(html.text))#extend - расширяет список
+                self.save_file(medications, FILE)
+            else:
+                print('Error')
+        except Exception as e:
+            print(e)
+    #parse('Грипп и простуда','Акушерство и гинекология.csv')
---
+++ main.py
@@ -0,0 +1,169 @@
+import telebot
+from telebot import types
+from Parser import Parser
+from ReadCatalog import ReadCatalog
+
+bot = telebot.TeleBot('')
+view = False
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    markup = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+    button1 = types.KeyboardButton('Лекарства')
+    button2 = types.KeyboardButton('Сбор информации')
+    # button1 = types.KeyboardButton('Обучить нейросеть')
+    # button2 = types.KeyboardButton('Загрузить изображение')
+    # button3 = types.KeyboardButton('Распознать')
+    markup.add(button1,button2)
+    bot.send_message(message.chat.id, "Выберите нужный!", reply_markup=markup)
+    global view
+    view = False
+
+@bot.message_handler(commands=['view'])
+def view(message):
+    get_catalog(message.from_user.id)
+    global view
+    view = True
+
+@bot.message_handler(commands=['medication'])
+def medication(message):
+    get_catalog(message.from_user.id)
+    global view
+    view = False
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    global view
+    CATALOG = ['Акушерство и гинекология', 'Аллергия', 'Инфекционные и вирусные заболевания', 'Дыхательная система',
+               'Грипп и простуда', 'Грипп и простуда', 'Противовоспалительные и обезболивающие средства',
+               'Желудочно-кишечный тракт и печень', 'Сердечно-сосудистая система',
+               'Неврологические и психические заболевания', 'Мочеполовая система и почки',
+               'Дерматологические заболевания', 'Онкологические заболевания', 'Эндокринные заболевания',
+               'Глазные и ушные капли', 'Лекарственные травы', 'Иммунитет', 'Гомеопатия', 'Обмен веществ',
+               'Препараты для анестезии, реанимации, трансфузий', 'Диабет', 'Спазмолитики']
+    print(message.text)
+    #по нажатию на кнопку "каталог"
+    if message.text == "Сбор информации":
+        get_catalog(message.from_user.id)
+    if message.text == "Лекарства":
+        get_catalog(message.from_user.id)
+        view = True
+
+    #проверяет текст на наличие в каталоге(список)
+    if message.text in CATALOG:
+        if view == False:
+            category = message.text
+            name_file = message.text.replace(' ','') + '.csv'
+            print(category + ' ' + name_file)
+            bot.send_message(message.from_user.id, text='Подождите... Идет сбор информации...')
+            Parser(message.from_user.id).parse(category,name_file)
+            bot.send_message(message.from_user.id, text='Сбор закончен!')
+        elif view == True:
+            name_file = message.text.replace(' ', '') + '.csv'
+            list_catalog = ReadCatalog().read_five_row(name_file)
+            if list_catalog:
+                for row in list_catalog:
+                    bot.send_message(message.from_user.id, text=row["Название"]+".\n Цена: "+row["Цена"])
+                    bot.send_photo(message.from_user.id, row["Ссылка на изображение"])
+                    print(row["Название"]+" "+row["Цена"]+" "+row["Ссылка на изображение"])
+            else:
+                bot.send_message(message.from_user.id, text="Соберите информацию для этой категории(/medication)")
+
+
+    if message.text == '/sstart':
+        keyboard = types.InlineKeyboardMarkup()  # наша клавиатура
+        key_head = types.InlineKeyboardButton(text='Голова', callback_data='head')  # кнопка «Да»
+        keyboard.add(key_head)  # добавляем кнопку в клавиатуру
+        key_stomach = types.InlineKeyboardButton(text='Живот', callback_data='stomach')
+        keyboard.add(key_stomach)
+
+        question = 'Выберите место, которое болит!'
+        bot.send_message(message.from_user.id, text=question, reply_markup=keyboard)
+        #--------Локация-----------------
+        # keyboardReply = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+        # button_geo = types.KeyboardButton(text="Отправить местоположение", request_location=True)
+        # keyboardReply.add(button_geo)
+        # bot.send_message(message.chat.id, reply_markup=keyboardReply)
+
+def get_catalog(id):
+    markup = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True, one_time_keyboard=True)
+    button1 = types.KeyboardButton('Акушерство и гинекология')
+    button2 = types.KeyboardButton('Аллергия')
+    button3 = types.KeyboardButton('Инфекционные и вирусные заболевания')
+    button4 = types.KeyboardButton('Дыхательная система')
+    button5 = types.KeyboardButton('Грипп и простуда')
+    button6 = types.KeyboardButton('Противовоспалительные и обезболивающие средства')
+    button7 = types.KeyboardButton('Желудочно-кишечный тракт и печень')
+    button8 = types.KeyboardButton('Сердечно-сосудистая система')
+    button9 = types.KeyboardButton('Неврологические и психические заболевания')
+    button10 = types.KeyboardButton('Мочеполовая система и почки')
+    button11 = types.KeyboardButton('Дерматологические заболевания')
+    button12 = types.KeyboardButton('Онкологические заболевания')
+    button13 = types.KeyboardButton('Эндокринные заболевания')
+    button14 = types.KeyboardButton('Глазные и ушные капли')
+    button15 = types.KeyboardButton('Лекарственные травы')
+    button16 = types.KeyboardButton('Иммунитет')
+    button17 = types.KeyboardButton('Гомеопатия')
+    button18 = types.KeyboardButton('Обмен веществ')
+    button19 = types.KeyboardButton('Препараты для анестезии, реанимации, трансфузий')
+    button20 = types.KeyboardButton('Диабет')
+    button21 = types.KeyboardButton('Спазмолитики')
+    markup.add(button1,button2,button3,button4,button5,button6,button7,button8,button9,button10,button11,button12, button13, button14,button15,button16,button17,button18,button19,button20,button21)
+    bot.send_message(id, "Лекарства", reply_markup=markup)
+
+def get_pills(call,name_of_pain):
+    stomach_pills = {
+        "1. Но-шпа":"Pictures\\No_shpa.jpg",
+        "2. Панкреатин":"Pictures\\pankreatin.jpg",
+        "3. Метоклопрамид":"Pictures\\metoclopramid.jpg",
+        "4. Маалокс":"Pictures\\maaloks.png",
+        "5. Дюспаталин":"Pictures\\duspatalin.jpg",
+        "6. Мотилиум":"Pictures\\motilium.jpg",
+
+    }
+    head_pills = {
+        "1. Панадол": "Pictures\\panadol.jpg",
+        "2. Спазмалгон": "Pictures\\spazmalgon.jpg",
+        "3. Ибупрофен": "Pictures\\ibuprofen.jpg",
+        "4. Аспирин С": "Pictures\\aspirin.jpg",
+        "5. Парацетамол": "Pictures\\paracetamol.jpg",
+        "6. Пенталгин": "Pictures\\pentalgin.jpg",
+
+    }
+    if name_of_pain == "stomach":
+        for key in stomach_pills:
+            print(key, '->', stomach_pills[key])
+            bot.send_message(call.message.chat.id, key)
+            #bot.send_photo(call.message.chat.id, stomach_pills[key])
+            bot.send_photo(call.message.chat.id, photo = open('{}'.format(stomach_pills[key]), 'rb'))
+    elif name_of_pain == "head":
+        for key in head_pills:
+            print(key, '->', head_pills[key])
+            bot.send_message(call.message.chat.id, key)
+            #bot.send_photo(call.message.chat.id, head_pills[key])
+            bot.send_photo(call.message.chat.id, photo=open('{}'.format(head_pills[key]), 'rb'))
+
+    # bot.send_photo(chat_id, photo=open('path', 'rb'))#путь к изображению
+
+
+# @bot.message_handler(content_types=["location"])
+# def location(message):
+#     if message.location is not None:
+#         print(message.location)
+#         print("latitude: %s; longitude: %s" % (message.location.latitude, message.location.longitude))
+
+# Обработчик нажатий на кнопки
+@bot.callback_query_handler(func=lambda call: True)
+def callback_worker(call):
+    if call.data == "head":  # call.data это callback_data, которую мы указали при объявлении кнопки
+        bot.send_message(call.message.chat.id, 'Головные боли')
+        get_pills(call, "head")
+    elif call.data == "stomach":
+        bot.send_message(call.message.chat.id, 'Боли в животе')
+        #---------------------------
+        get_pills(call,"stomach")
+    elif call.data =="category":
+        bot.send_message(call.message.chat.id,"Категории:")
+
+
+bot.polling(none_stop=True, interval=0)
